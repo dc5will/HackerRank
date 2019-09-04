4eaa852df5f1f60dac3576ce94c4c86a9a84a1c5
William Wong <wwong1987@gmail.com>
2019-09-04 10:19:19 -0700

    completed jumpingOnClouds problem brute force naive solution

--- warmupChallenges.js
+++ warmupChallenges.js
@@ -121,3 +121,31 @@ function countingValleys2(n, s) {
 }
 
 console.log(countingValleys(8, "UDDDUDUU")); // returns 1
+
+// HACK: Jumping on the Clouds - Emma is playing a new mobile game that starts with consecutively numbered clouds. Some of the clouds are thunderheads and others are cumulus. She can jump on any cumulus cloud having a number that is equal to the number of the current cloud plus  or . She must avoid the thunderheads. Determine the minimum number of jumps it will take Emma to jump from her starting postion to the last cloud. It is always possible to win the game.
+
+// For each game, Emma will get an array of clouds numbered  if they are safe or  if they must be avoided. For example, indexed from . The number on each cloud is its index in the list so she must avoid the clouds at indexes  and . She could follow the following two paths:  or . The first path takes  jumps while the second takes .
+
+// 0 = safe, 1 = avoid. set count variable. iterate array with condition that if arr[i] === 0 then i++
+
+function jumpingOnClouds(c) {
+  let count = 1;
+  for (let i = 0; i < c.length; i++) {
+    console.log('steps =', c[i], c[i+1], 'count =', count);
+    if (c[i] === 0 && c[i+1] === 1) {
+      count++;
+      i++;
+    } else if (c[i] === 0 && c[i+1] === 0 && c[i+2] === 0) {
+      count++;
+      i++
+    } else if (c[i] === 0 && c[i+1] === 0) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+  return count;
+}
+
+console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0])); // returns 4
+console.log(jumpingOnClouds([0, 0, 0, 1, 0, 0])); // returns 3
