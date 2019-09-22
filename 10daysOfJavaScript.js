bf2ac8b05a8b0292dc637490c8443070a6c3c2fd
William Wong <wwong1987@gmail.com>
2019-09-22 16:39:41 -0700

    completed getGrade problem

--- 10daysOfJavaScript.js
+++ 10daysOfJavaScript.js
@@ -29,3 +29,39 @@ function main() {
   // Print the perimeter of the circle:
   console.log(PI * 2 * r);
 }
+
+/* HACK: Conditional Statements: If-else
+Complete the getGrade(score) function in the editor. It has one parameter: an integer, score , denoting the number of points Julia earned on an exam. It must return the letter corresponding to her grade according to the following rules:
+
+If 25 < score <= 30, then grade = A.
+If 20 < score <= 25, then grade = B.
+If 15 < score <= 20, then grade = C.
+If 10 < score <= 15, then grade = D.
+If 5 < score <= 10, then grade = E.
+If 0 < score <= 5, then grade = F.
+*/
+
+function getGrade(score) {
+  let grade;
+  if (score > 25 && score <= 30) {
+    grade = 'A';
+  }
+  if (score > 20 && score <= 25) {
+    grade = 'B';
+  }
+  if (score > 15 && score <= 20) {
+    grade = 'C';
+  }
+  if (score > 10 && score <= 15) {
+    grade = 'D';
+  }
+  if (score > 5 && score <= 10) {
+    grade = 'E';
+  }
+  if (score > 0 && score <= 5) {
+    grade = 'F';
+  }
+  return grade;
+}
+
+console.log(getGrade(11)) // returns D;
