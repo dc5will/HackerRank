c2341c1b26ce72876782cfac1e046e089b938547
William Wong <wwong1987@gmail.com>
2019-09-22 19:30:49 -0700

    completed getLetter switch condition problem

--- 10daysOfJavaScript.js
+++ 10daysOfJavaScript.js
@@ -140,3 +140,24 @@ l
 p
 s
 */
+
+/* HACK: Conditional Statements: Switch
+Complete the getLetter(s) function in the editor. It has one parameter: a string, , consisting of lowercase English alphabetic letters (i.e., a through z). It must return A, B, C, or D depending on the following criteria:
+
+If the first character in string s is in the set {a,e,i,o,u}, then return A.
+If the first character in string s is in the set {b,c,d,f,g}, then return B.
+If the first character in string s is in the set {h,j,k,l,m}, then return C.
+If the first character in string s is in the set {n,p,q,r,s,t,v,w,x,y,z}, then return D.
+Hint: You can get the letter at some index i in s using the syntax s[i] or s.charAt(i).
+*/
+
+// grab first item and check for the sets that contain it and return the proper output
+
+function getLetter(s) {
+  let letter = s[0];
+  console.log(letter);
+
+  return letter;
+}
+
+console.log(getLetter('adfgt')) // return A
