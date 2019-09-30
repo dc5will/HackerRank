398a67663940a1a6568dbd6297c850e1a7ef19ca
William Wong <wwong1987@gmail.com>
2019-09-29 17:26:34 -0700

    completed reverseString try/catch/finally problem

--- 10daysOfJavaScript.js
+++ 10daysOfJavaScript.js
@@ -239,3 +239,29 @@ function getSecondLargest2(nums) {
 }
 
 console.log(getSecondLargest1([2,3,6,6,5])) // returns 5
+
+/* HACK: Try, Catch, and Finally
+Complete the reverseString function; it has one parameter, s. You must perform the following actions:
+
+1. Try to reverse string s using the split, reverse, and join methods.
+2. If an exception is thrown, catch it and print the contents of the exception's message on a new line.
+3. Print s on a new line. If no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
+
+Must print two print statements using console.log();
+1. Print the contents of a caught exception's message on a new line. If no exception was thrown, this line should not be printed.
+2. Print s on a new line. If no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
+
+Initial Approach: Wrap reverse string in a try block. Catch exceptions by logging out message in a catch block.  
+*/
+
+function reverseString(s) {
+  try {
+    console.log(s.split('').reverse().join(''));
+  } catch(e) {
+    console.log(e.message);
+    console.log(s);
+  }
+}
+
+console.log(reverseString("1234")) // returns 4321
+console.log(reverseString(Number(1234))) // throws error
