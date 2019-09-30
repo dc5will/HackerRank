af81020ca0fe6ef15ffeef650ab9fb50f9ff0a88
William Wong <wwong1987@gmail.com>
2019-09-30 09:38:53 -0700

    completed getCount problem (iterate through array of objects)

--- 10daysOfJavaScript.js
+++ 10daysOfJavaScript.js
@@ -364,3 +364,19 @@ function Rectangle(a,b) {
 }
 
 console.log(Rectangle(4,5)) // 18, 20
+
+// HACK: Count Objects - Complete the function in the editor. It has one parameter: an array, a , of objects. Each object in the array has two integer properties denoted by x and y. The function must return a count of all such objects o in array a that satisfy o.x == o.y.
+
+function getCount(objects) {
+  let count = 0;
+  for (let i = 0; i < objects.length; i++) {
+    if (objects[i].x === objects[i].y) {
+      // console.log('objects[i].x=', objects[i].x)
+      // console.log('objects[i].y=', objects[i].y)
+      count++;
+    }
+  }
+  return count;
+}
+
+
