6d1e3f4fb96f47afd1582529380fe919b982083a
William Wong <wwong1987@gmail.com>
2019-09-29 17:45:38 -0700

    completed isPositive problem (throw problem) with alternative solutions

--- 10daysOfJavaScript.js
+++ 10daysOfJavaScript.js
@@ -276,3 +276,19 @@ function reverseString1(s) {
 
 console.log(reverseString("1234")) // returns 4321
 console.log(reverseString(Number(1234))) // throws error
+
+/* HACK: Throw - Complete the isPositive function below. It has one integer parameter, a. If the value of a is positive, it must return the string YES. Otherwise, it must throw an Error according to the following rules:
+
+If a is 0, throw an Error with message = Zero Error.
+If a is negative, throw an Error with message = Negative Error.
+*/
+
+function isPositive(a) {
+  if (a === 0) throw Error('Zero Error');
+  if (a < 0) throw Error('Negative Error');
+  return 'YES'
+}
+
+// console.log(isPositive(3)) // returns YES
+// console.log(isPositive(0)) // returns Zero Error
+// console.log(isPositive(-1)) // returns Negative Error
