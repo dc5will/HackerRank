45b21b5c10efea30923731eea7bbc7f44e3dfd64
William Wong <wwong1987@gmail.com>
2019-10-01 11:52:37 -0700

    completed countSwaps bubbleSort problem

--- sorting.js
+++ sorting.js
@@ -29,6 +29,21 @@ Given an array of integers, sort the array in ascending order using the Bubble S
 
 */
 
-function countSwaps(a) {
+// console.log(swap(1,2)) // returns [2,1]
 
+function countSwaps(a) {
+  let count = 0;
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < a.length; j++) {
+      if (a[j] > a[j+1]) {
+        [a[j], a[j+1]] = [a[j+1],a[j]];
+        count++
+      }
+    }
+  }
+  console.log(`Array is sorted in ${count} swaps.`)
+  console.log(`First Element: ${a[0]}`)
+  console.log(`Last Element: ${a[a.length-1]}`)
 }
+
+console.log(countSwaps([6,4,1])) // returns 3;
