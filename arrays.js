2b39e4b99b708b292caf5ca41629595f08cc06d6
William Wong <wwong1987@gmail.com>
2019-10-04 21:51:42 -0700

    completed compareTriplets

--- arrays.js
+++ arrays.js
@@ -64,3 +64,20 @@ function simpleArraySum(ar) {
 }
 
 console.log(simpleArraySum([1, 2, 3, 4, 10, 11])) // returns 31; 
+
+
+// HACK: Compare the Triplets
+function compareTriplets(a, b) {
+  let countA = 0;
+  let countB = 0;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > b[i]) {
+      countA++;
+    } else if (a[i] < b[i]) {
+      countB++;
+    }
+  }
+  return [countA, countB];
+}
+
+console.log(compareTriplets([5,6,7],[3,6,10]));
