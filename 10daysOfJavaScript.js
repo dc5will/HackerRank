9806f5f472e984dd5888d6aaaf5e3736acf0acd6
William Wong <williamwdev@gmail.com>
2019-11-05 20:02:23 -0800

    completed class inheritance problem

--- 10daysOfJavaScript.js
+++ 10daysOfJavaScript.js
@@ -429,7 +429,7 @@ class Polygon {
   }
 }
 
-// REVIEW: Alternative solutions
+// REVIEW: Alternative solution
 class Polygon {
   constructor(sides) {
     this.sides = sides;
@@ -443,3 +443,34 @@ class Polygon {
 
 let triangle = new Polygon([3, 4, 5]);
 console.log(triangle.perimeter());
+
+/* 
+HACK: Inheritance - We provide the implementation for a Rectangle class in the editor. Perform the following tasks:
+Add an area method to Rectangle's prototype.
+Create a Square class that satisfies the following:
+It is a subclass of Rectangle.
+It contains a constructor and no other methods.
+It can use the Rectangle class' area method to print the area of a Square object.
+*/
+
+class Rectangle {
+  constructor(w, h) {
+    this.w = w;
+    this.h = h;
+  }
+}
+/*
+ *  Write code that adds an 'area' method to the Rectangle class' prototype
+ */
+Rectangle.prototype.area = function() {
+  return (this.w * this.h);
+};
+
+/*
+ * Create a Square class that inherits from Rectangle and implement its class constructor
+ */
+class Square extends Rectangle {
+  constructor(sides) {
+    super(sides, sides);
+  }
+}
